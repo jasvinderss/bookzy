88a9aa16a2b424f5b9735f662ecbd8812f866aab
Jasvinder <jasvinder-s@outlook.com>
2024-02-22 11:15:02 +0530

    Added Bookzy Springboot

---
+++ bookzyProject_zip/src/main/java/com/bookzy/bookzyProject/BookzyProjectApplication.java
@@ -0,0 +1,13 @@
+package com.bookzy.bookzyProject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookzyProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookzyProjectApplication.class, args);
+	}
+
+}
---
+++ bookzyProject_zip/src/test/java/com/bookzy/bookzyProject/BookzyProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bookzy.bookzyProject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookzyProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
